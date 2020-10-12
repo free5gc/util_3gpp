ba6b518306bd0b6d348801d407b0a3a0ffc81650
am677 <mishima@iij.ad.jp>
2020-10-12 14:52:03 +0900

    Add swap processing for supiType IMSI

--- suci/toSupi.go
+++ suci/toSupi.go
@@ -15,6 +15,7 @@ import (
 	"log"
 	"math"
 	"math/big"
+	"math/bits"
 	"strings"
 
 	"golang.org/x/crypto/curve25519"
@@ -128,7 +129,15 @@ func AnsiX963KDF(sharedKey, publicKey []byte, profileEncKeyLen, profileMacKeyLen
 	return kdfKey
 }
 
-func profileA(input string) (string, error) {
+func swapNibbles(input []byte) []byte {
+	output := make([]byte, len(input))
+	for i, b := range input {
+		output[i] = bits.RotateLeft8(b, 4)
+	}
+	return output
+}
+
+func profileA(input, supiType string) (string, error) {
 	logger.UeauLog.Infoln("SuciToSupi Profile A")
 	s, hexDecodeErr := hex.DecodeString(input)
 	if hexDecodeErr != nil {
@@ -182,10 +191,20 @@ func profileA(input string) (string, error) {
 
 	decryptPlainText := Aes128ctr(decryptCipherText, decryptEncKey, decryptIcb)
 
-	return hex.EncodeToString(decryptPlainText), nil
+	var schemeResult string
+	if supiType == typeIMSI {
+		schemeResult = hex.EncodeToString(swapNibbles(decryptPlainText))
+		if schemeResult[len(schemeResult)-1] == 'f' {
+			schemeResult = schemeResult[:len(schemeResult)-1]
+		}
+	} else {
+		schemeResult = hex.EncodeToString(decryptPlainText)
+	}
+
+	return schemeResult, nil
 }
 
-func profileB(input string) (string, error) {
+func profileB(input, supiType string) (string, error) {
 	logger.UeauLog.Infoln("SuciToSupi Profile B")
 	s, hexDecodeErr := hex.DecodeString(input)
 	if hexDecodeErr != nil {
@@ -266,7 +285,17 @@ func profileB(input string) (string, error) {
 
 	decryptPlainText := Aes128ctr(decryptCipherText, decryptEncKey, decryptIcb)
 
-	return hex.EncodeToString(decryptPlainText), nil
+	var schemeResult string
+	if supiType == typeIMSI {
+		schemeResult = hex.EncodeToString(swapNibbles(decryptPlainText))
+		if schemeResult[len(schemeResult)-1] == 'f' {
+			schemeResult = schemeResult[:len(schemeResult)-1]
+		}
+	} else {
+		schemeResult = hex.EncodeToString(decryptPlainText)
+	}
+
+	return schemeResult, nil
 }
 
 // suci-0(SUPI type)-mcc-mnc-routingIndentifier-protectionScheme-homeNetworkPublicKeyIdentifier-schemeOutput.
@@ -311,14 +340,14 @@ func ToSupi(suci string) (string, error) {
 	}
 
 	if scheme == profileAScheme {
-		profileAResult, err := profileA(suciPart[len(suciPart)-1])
+		profileAResult, err := profileA(suciPart[len(suciPart)-1], suciPart[supiTypePlace])
 		if err != nil {
 			return "", err
 		} else {
 			return supiPrefix + mccMnc + profileAResult, nil
 		}
 	} else if scheme == profileBScheme {
-		profileBResult, err := profileB(suciPart[len(suciPart)-1])
+		profileBResult, err := profileB(suciPart[len(suciPart)-1], suciPart[supiTypePlace])
 		if err != nil {
 			return "", err
 		} else {
